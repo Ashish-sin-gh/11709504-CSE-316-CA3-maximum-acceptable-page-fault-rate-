92fd75b20f7e6e4e9219b04d96a79297896ab106
AshishIndia <42372004+AshishIndia@users.noreply.github.com>
2019-04-22 22:45:41 +0530

    update1

---
+++ pageFault.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#includestdlib.h>
+##include<conio.h>
+
+double max_page_fault_rate();
+data_in()
+{
+printf("\nEnter service Page Fault");
+	scanf("%lf",&service_page_fault_empty);
+	printf("Enter Service Page Fault");
+	scanf("%lf",&service_page_fault_modified);
+}
+
