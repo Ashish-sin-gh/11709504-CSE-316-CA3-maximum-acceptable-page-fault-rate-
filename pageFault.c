46661ecfe0dcadde2e7ed489263b847a0b4755a6
AshishIndia <42372004+AshishIndia@users.noreply.github.com>
2019-04-23 21:06:34 +0530

    update2

--- pageFault.c
+++ pageFault.c
@@ -3,11 +3,30 @@
 ##include<conio.h>
 
 double max_page_fault_rate();
-data_in()
+
+double pageFaultEmpty;
+double pageFaultModified;
+double accessTime;
+double pageModTime;
+double effectiveAccessTime;
+double pageFaultRate;
+double service_page_fault_empty_ns;
+double service_page_fault_modified_ns;
+double tpm;
+
+dataIn()
 {
-printf("\nEnter service Page Fault");
-	scanf("%lf",&service_page_fault_empty);
-	printf("Enter Service Page Fault");
-	scanf("%lf",&service_page_fault_modified);
+printf("\nEnter service Page Fault | Empty | in millisecond");
+	scanf("%lf",&pageFaultEmpty);
+	printf("Enter Service Page Fault | Modified Page | in millisecond");
+	scanf("%lf",&pageFaultModified);
+	printf("Enter Memory Access Time | in nanosecond");
+	scanf("%lf",&accessTime);
+	printf("Enter % of time required to page to be replaced [0-100]");
+	scanf("%lf",&pageModTime);
+	printf("Enter Effective Access time | in nanosecond");
+	scanf("%lf",&effectiveAccessTime);
+	
+	
 }
 
