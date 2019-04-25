938b86b3dd1164de3b0f0247948e8c48dd11df87
AshishIndia <42372004+AshishIndia@users.noreply.github.com>
2019-04-25 06:41:34 +0530

    update4

--- pageFault.c
+++ pageFault.c
@@ -28,7 +28,7 @@ void main(){
 		break;
 		case 2:exit(0);
 	}
-	printf("\n\n");
+	printf("\n\n\n\n");
 
 }
 while(z<3);
@@ -36,47 +36,41 @@ while(z<3);
 
 void userInput(){
 	printf("\nEnter service Page Fault | Empty | in millisecond");
-
 	scanf("%lf",&pageFaultEmpty);
-
-	printf("Enter Service Page Fault | Modified Page | in millisecond");
-	
+	printf("Enter Service Page Fault | Modified Page | in millisecond");	
 scanf("%lf",&pageFaultModified);
-
 	printf("Enter Memory Access Time | in nanosecond");
-	
 scanf("%lf",&accessTime);
-
-	printf("Enter % of time required to page to be replaced [0-100]");
-
+	printf("Enter percentage of time required to page to be replaced [0-100]");
 	scanf("%lf",&pageModTime);
-
 	printf("Enter Effective Access time | in nanosecond");
-
 	scanf("%lf",&effectiveAccessTime);
-	
-
 	PFE = (pageFaultEmpty*1000000);
-
 	PFM = (pageFaultModified*1000000);
-
 	tpm = (pageModTime/100);
-   
 	printf("\nPage Fault rate calculated For:\n");
-
 	printf("Page Fault for Empty page (in nanoseconds): %lf \n",PFE);
-
 	printf("Page Fault for Modified Page (in nanoseconds): %lf \n",PFM);
-
 	printf("Memory Access Time(in nanoseconds): %lf\n",accessTime);
-
 	printf("Effective Access Time %lf\n",effectiveAccessTime);
-   
-                  pageFaultRate =  page_fault_rate(PFE,PFM,accessTime,tpm,effectiveAccessTime);
-
+        pageFaultRate =  page_fault_rate(PFE,PFM,accessTime,tpm,effectiveAccessTime);
 	printf("\nMaximum Acceptable Page Fault rate = %.2e[exponential notation]",pageFaultRate);
+}
 
+double page_fault_rate(double servicePageFaultEmpty, double servicePageFaultMod, double MA , double timesPages,double EA)
+{
+	double a;
+                  double b;
+	double n;
+                  double d;
+	double PF;
+	 a = (1- timesPages)*servicePageFaultEmpty;
+	 b = timesPages*servicePageFaultMod;
+	n =EA -MA;
+     d = (a+b);
 
+	PF =n/d;
+	return PF;
 }
 
 
