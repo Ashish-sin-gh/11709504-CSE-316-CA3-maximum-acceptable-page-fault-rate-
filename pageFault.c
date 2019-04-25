cd033764aba4b0fd420ff0d6011f9a9f4e42a571
AshishIndia <42372004+AshishIndia@users.noreply.github.com>
2019-04-25 06:46:25 +0530

    update5

--- pageFault.c
+++ pageFault.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<conio.h>
-double page_fault_rate();
-void userInput(void);
 
 double pageFaultEmpty;
 double pageFaultModified;
@@ -13,64 +11,74 @@ double pageFaultRate;
 double PFE;
 double PFM;
 double tpm;
+
+double page_fault_rate(double PageFaultEmpty, double PageFaultMod, double MA , double timesPages,double EA)
+{
+	double a;
+    double b;
+	double n;
+    double d;
+	double PF;
+	a = (1- timesPages) * PageFaultEmpty;
+	b = timesPages * PageFaultMod;
+	n =EA - MA;
+    d = (a+b);
+	PF =n/d;
+	return PF;
+
+}
+
+userInput()
+{
+printf("\nEnter service Page Fault | Empty | in millisecond\n");
 	
+	scanf("%lf",&pageFaultEmpty);
+	printf("Enter Service Page Fault | Modified Page | in millisecond\n");
+	
+scanf("%lf",&pageFaultModified);
+	printf("Enter Memory Access Time | in nanosecond\n");
+scanf("%lf",&accessTime);
+
+	printf("Enter percentage of page to be replaced [0-100]\n");
+	scanf("%lf",&pageModTime);
+
+	printf("Enter Effective Access time | in nanosecond\n");
+	scanf("%lf",&effectiveAccessTime);
+	
+	PFE = (pageFaultEmpty*1000000);
+	PFM = (pageFaultModified*1000000);
+	tpm = (pageModTime/100);
+   
+	printf("\nPage Fault rate calculated For:\n");
+	printf("Page Fault (Page Not Modified): %lf \n",PFE);
+	printf("Page Fault (Modified Page): %lf \n",PFM);
+	printf("Memory Access Time(ns): %lf\n",accessTime);
+	printf("Effective Access Time: %lf\n",effectiveAccessTime);
+    	pageFaultRate =  page_fault_rate(PFE,PFM,accessTime,tpm,effectiveAccessTime);
+	printf("\nMaximum Acceptable Page Fault rate: %.3e",pageFaultRate);
+}
+
 
 void main(){
+	
 	int z;
 	
-	do{
+	
+	A:
 	printf("press desired key:\n");
 	printf("   1 for Finding the maximum PageFault Rate\n");
 	printf("   2  for closing the program");
 	scanf("%d",&z);
 	switch(z){
 		case 1:userInput();
+		printf("\n\n\n");
+		goto A;
 		break;
 		case 2:exit(0);
 	}
-	printf("\n\n\n\n");
-
-}
-while(z<3);
+	
 }
 
-void userInput(){
-	printf("\nEnter service Page Fault | Empty | in millisecond");
-	scanf("%lf",&pageFaultEmpty);
-	printf("Enter Service Page Fault | Modified Page | in millisecond");	
-scanf("%lf",&pageFaultModified);
-	printf("Enter Memory Access Time | in nanosecond");
-scanf("%lf",&accessTime);
-	printf("Enter percentage of time required to page to be replaced [0-100]");
-	scanf("%lf",&pageModTime);
-	printf("Enter Effective Access time | in nanosecond");
-	scanf("%lf",&effectiveAccessTime);
-	PFE = (pageFaultEmpty*1000000);
-	PFM = (pageFaultModified*1000000);
-	tpm = (pageModTime/100);
-	printf("\nPage Fault rate calculated For:\n");
-	printf("Page Fault for Empty page (in nanoseconds): %lf \n",PFE);
-	printf("Page Fault for Modified Page (in nanoseconds): %lf \n",PFM);
-	printf("Memory Access Time(in nanoseconds): %lf\n",accessTime);
-	printf("Effective Access Time %lf\n",effectiveAccessTime);
-        pageFaultRate =  page_fault_rate(PFE,PFM,accessTime,tpm,effectiveAccessTime);
-	printf("\nMaximum Acceptable Page Fault rate = %.2e[exponential notation]",pageFaultRate);
-}
 
-double page_fault_rate(double servicePageFaultEmpty, double servicePageFaultMod, double MA , double timesPages,double EA)
-{
-	double a;
-                  double b;
-	double n;
-                  double d;
-	double PF;
-	 a = (1- timesPages)*servicePageFaultEmpty;
-	 b = timesPages*servicePageFaultMod;
-	n =EA -MA;
-     d = (a+b);
-
-	PF =n/d;
-	return PF;
-}
 
 
